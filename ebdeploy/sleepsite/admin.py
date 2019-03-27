90a13b2cd609853940b9ef41cb6bf65b27c1624c
gracez72 <gracezhang72@gmail.com>
2019-03-26 18:01:47 -0700

    test again

--- ebdeploy/sleepsite/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Alarm, SleepData, Event, Song
-
-# Register your models here.
-admin.site.register(Alarm)
-admin.site.register(SleepData)
-admin.site.register(Event)
-admin.site.register(Song)
-
